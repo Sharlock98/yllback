866cf7dca2a339e40ff1b830e46d8bbfc0300760
Sharlock98 <www.674590711@qq.com>
2019-05-21 17:57:57 +0800

    5.21

---
+++ src/main/java/com/example/yllback/dao/AdministratorDao.java
@@ -0,0 +1,22 @@
+package com.example.yllback.dao;
+
+import com.example.yllback.entity.Administrator;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AdministratorDao {
+    List<Administrator> selectByaccount(@Param("account") String account,@Param("password") String password);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Administrator record);
+
+    int insertSelective(Administrator record);
+
+    Administrator selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Administrator record);
+
+    int updateByPrimaryKey(Administrator record);
+}
---
+++ src/main/java/com/example/yllback/entity/Administrator.java
@@ -0,0 +1,82 @@
+package com.example.yllback.entity;
+
+import java.io.Serializable;
+
+/**
+ * administrator
+ * @author 
+ */
+public class Administrator implements Serializable {
+    private Integer id;
+
+    private String account;
+
+    private String password;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Administrator other = (Administrator) that;
+        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
+            && (this.getAccount() == null ? other.getAccount() == null : this.getAccount().equals(other.getAccount()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
+        result = prime * result + ((getAccount() == null) ? 0 : getAccount().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", account=").append(account);
+        sb.append(", password=").append(password);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
